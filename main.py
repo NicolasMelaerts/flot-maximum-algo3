f50db255192b6f3c6a5537450d136ce0fbbf2c78
nmelaert <nicolas.melaerts@icloud.com>
2023-04-26 17:01:18 +0200

    [ADD] readInstance method

--- main.py
+++ main.py
@@ -9,8 +9,22 @@ But du projet :
 """
 
 
+def readInstance(file_path):
+    with open(file_path, "r") as file:
+        lines = file.readlines()
+        nodes = int(lines[0].split()[1])
+        source = int(lines[1].split()[1])
+        sink = int(lines[2].split()[1])
+        arcs = int(lines[3].split()[1])
+        capacities = [[0 for _ in range(nodes)] for _ in range(nodes)]
+        for line in lines[4:]:
+            i, j, c = line.split()
+            capacities[int(i)][int(j)] = int(c)
+    return nodes, source, sink, arcs, capacities
+
+
 def main():
-    pass
+    print(readInstance("Instances/inst-100-0.1.txt"))
 
 
 if __name__ == '__main__':
